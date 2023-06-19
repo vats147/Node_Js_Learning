29d7c6ecd52619306ba050906186735f40fa42e8
vats <vatsalmangukiya9003@gmail.com>
2023-06-19 22:52:25 +0530

    complete pug understanding + 007 video complete

---
+++ 6. Working with Dynamic Content & Adding Templating Engines/routes/shop.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+const adminData =require('./admin');
+router.get('/', (req, res, next) => {
+  // console.log('shop.js',adminData.products);
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+
+  const products=adminData.products;
+
+  res.render('shop',{prods:products,title:'mytitle'});
+});
+
+module.exports = router;
